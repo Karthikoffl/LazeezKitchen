3866dec1ee978874cb2372da67441259f78ea88d
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-06-15 11:25:07 +0530

    (1506) commit

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -23,11 +23,25 @@ import {
 import { TextInput } from "react-native-paper";
 import { AuthenticationContext } from "../authentication/authentication.context";
 import { ActivityIndicator, MD2Colors } from "react-native-paper";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from "expo-auth-session";
+import * as Facebook from "expo-auth-session/providers/facebook";
 
 const Login = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(true);
+  WebBrowser.maybeCompleteAuthSession();
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId:
+      "586012277192-a1nqhnurk2rvkbl927unjj9rlcqi0532.apps.googleusercontent.com",
+    expoClientId:
+      "586012277192-f68aokhq14c500icmakro6hj9m0dpjp6.apps.googleusercontent.com",
+  });
+  const [req, res, fblogin] = Facebook.useAuthRequest({
+    clientId: "1848014555585292",
+  });
 
   const {
     onLogin,
@@ -108,10 +122,10 @@ const Login = ({ navigation }) => {
         {/* <TouchableOpacity onPress={() => {}}>
           <FontAwesome name="envelope" size={24} color="gray" />
         </TouchableOpacity> */}
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity onPress={() => promptAsync()}>
           <Image source={require("../assets/images/gmail.png")} />
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity onPress={() => fblogin()}>
           <Image source={require("../assets/images/fb.png")} />
         </TouchableOpacity>
       </SocialLoginView>
